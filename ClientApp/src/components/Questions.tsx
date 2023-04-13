34e4390ddd8188c9990d83e5625cb50674423782
Shubhangi Chauhan <Shubhangi Chauhan@LAPTOP-P6VV32JO>
2023-04-13 15:07:12 +0530

    Questions added

--- ClientApp/src/components/Questions.tsx
+++ ClientApp/src/components/Questions.tsx
@@ -1,9 +1,8 @@
-﻿import React, { useEffect,useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror';
 import 'codemirror/theme/dracula.css';
 import axios from "axios";
-
 
 type _Question = {
     _id: string;
@@ -15,17 +14,20 @@ type _Question = {
     testCase1: string;
     testCase2: string;
     testCase3: string;
-
 };
 
 const Questions = () => {
     let [_Questions, setQuestions] = useState<Array<_Question>>([]);
 
-    axios.get("https://localhost:44322/api/Editor").then(response => {
-        setQuestions(response.data);
-    }).catch(err => {
-        console.log(err)
-    })
+    useEffect(() => {
+        axios.get("https://localhost:44322/api/Editor")
+            .then(response => {
+                setQuestions(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
 
     useEffect(() => {
         const editor = CodeMirror.fromTextArea(document.getElementById('editor') as HTMLTextAreaElement, {
@@ -36,13 +38,9 @@ const Questions = () => {
     }, []);
 
 
-
-    console.log(_Questions);
-
     const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const handleNextQuestion= () => {
-        
+    const handleNextQuestion = () => {
         const nextQuestion = currentQuestion + 1;
         if (nextQuestion < _Questions.length) {
             setCurrentQuestion(nextQuestion);
@@ -50,44 +48,34 @@ const Questions = () => {
             setCurrentQuestion(0);
         }
     };
-    console.log(_Questions.length);
-    
+
+
     return (
         <div className="row">
-            <div col-sm-6>
+            <div className="col-sm">
                 <textarea id='editor'
                     placeholder="Type your code here" />
             </div>
-            <div col-sm-6>
-                    (
-                        <>
-                            <div className='question-section'>
-                        <div className='question-count'>
-                            <div>{_Questions.length}</div>
+            <div className="col-sm">
+                {(_Questions && _Questions.length > 0) && (
+                    <div className='question-section'>
+                        <div className='question-count'></div>
+                        <div>
+                            <span>question {currentQuestion + 1}</span>
+                            <div className='question-text'>{_Questions[currentQuestion]?.question}</div>
+                            <button
+                                type="button"
+                                className="btn btn-warning"
+                                onClick={() => handleNextQuestion()}
+                            >
+                                Next
+                            </button>
                         </div>
-                        if({_Questions.length>0}){<div>
-                            <span>Question {currentQuestion + 1}</span>
-                            
-                            <div className='question-text'>{_Questions[currentQuestion].question}</div>
-                        <button
-                            type="button"
-                            className="btn btn-warning"
-                            onClick={() => handleNextQuestion()}
-                        >
-                           Next
-                        </button></div>}
-                                    
-                                
-                        
-                            </div>
-                        </>
-                    )
+                    </div>
+                )}
             </div>
-            );
-            </div>
-            
-        )
-
+        </div>
+    );
 };
 
 export default Questions;
