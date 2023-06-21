19bd3550d406fc337165fdeb2835f8a5df672a68
Shubhangi Chauhan <Shubhangi Chauhan@LAPTOP-P6VV32JO>
2023-06-21 20:48:57 +0530

    Changes for all the variable types

---
+++ CodeFiles/634ad75abe6e466da7cdc3f82f01f733.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int solution(int a,int b) {
+  // Your code here
+  int ans = a+b;
+  return ans;
+}
+
+int main() {
+    int a,b,ans;
+    cin>>a>>b;
+  ans = solution(a,b);
+  cout<<ans;
+  return 0;
+}
